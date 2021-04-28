727034d00a715b0dfa5be0602a08cb69e4a1b423
Jess Planck <jesse@funroe.net>
2021-04-28 13:23:16 -0400

    Updates after getting phpcs and WordPress coding standates working with the Atom linter.

--- jess-editor-block-test.php
+++ jess-editor-block-test.php
@@ -10,21 +10,22 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: jess-block-test
  * Domain Path: /languages
+
+ * @package jess-block-test
  */
 
- /*
- * Notes
- * Notes from https://github.com/modularwp/gutenberg-block-editable-example
- * Block Icons https://developer.wordpress.org/resource/dashicons/#block-default
- *
- * Javascript internationalization in WordPress https://make.wordpress.org/core/2018/11/09/new-javascript-i18n-support-in-wordpress/
- *
- * Note JS diffences for ES5 and ESNext on Wordpress.org documentation
- * https://developer.wordpress.org/block-editor/reference-guides/richtext/
- * WordPress block types - Block editor handbook
- * https://developer.wordpress.org/block-editor/how-to-guides/block-tutorial/writing-your-first-block-type/
- *
- */
+ /**
+  * Notes
+  * Notes from https://github.com/modularwp/gutenberg-block-editable-example
+  * Block Icons https://developer.wordpress.org/resource/dashicons/#block-default
+  *
+  * Javascript internationalization in WordPress https://make.wordpress.org/core/2018/11/09/new-javascript-i18n-support-in-wordpress/
+  *
+  * Note JS diffences for ES5 and ESNext on Wordpress.org documentation
+  * https://developer.wordpress.org/block-editor/reference-guides/richtext/
+  * WordPress block types - Block editor handbook
+  * https://developer.wordpress.org/block-editor/how-to-guides/block-tutorial/writing-your-first-block-type/
+  */
 
 // Security - Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -37,7 +38,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since 1.0.0
  */
 function jess_editor_block_test_css_enqueue() {
-	wp_register_style( 'jess-editor-block-test-css', plugins_url( 'jess-editor-block-test.css', __FILE__ ),'','', 'screen' );
+	wp_register_style( 'jess-editor-block-test-css', plugins_url( 'jess-editor-block-test.css', __FILE__ ), '', '1.0', 'screen' );
 	wp_enqueue_style( 'jess-editor-block-test-css' );
 }
 add_action( 'wp_enqueue_scripts', 'jess_editor_block_test_css_enqueue' );
@@ -49,16 +50,16 @@ add_action( 'wp_enqueue_scripts', 'jess_editor_block_test_css_enqueue' );
  */
 function jess_editor_block_test_js_enqueue() {
 
-	// Javascript for editor block. See jess-editor-block-test.js
+	/** Javascript for editor block. See jess-editor-block-test.js */
 	wp_enqueue_script(
 		'mdlr-editable-block-example-backend-script', // Unique handle.
-		plugins_url( 'jess-editor-block-test.js', __FILE__ ), // WP Editor javascript
+		plugins_url( 'jess-editor-block-test.js', __FILE__ ), // WP Editor javascript.
 		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Note dependencies, see note links.
 		filemtime( plugin_dir_path( __FILE__ ) . 'jess-editor-block-test.js' ) // filemtime — Gets file modification time.
 	);
 
 	// Enqueue hover effect CSS so it can be seen while editing. NOTE: future testing for span vs. div testing.
-	wp_register_style( 'jess-editor-block-test-css', plugins_url( 'jess-editor-block-test.css', __FILE__ ),'','', 'screen' );
+	wp_register_style( 'jess-editor-block-test-css', plugins_url( 'jess-editor-block-test.css', __FILE__ ), '', '1.0', 'screen' );
 	wp_enqueue_style( 'jess-editor-block-test-css' );
 }
 add_action( 'enqueue_block_editor_assets', 'jess_editor_block_test_js_enqueue' );
